fc35d1b44f9bd0f1981b1ef93829fc4cb0a97c6a
Alec Gonzales <alec.gonzales@orangeandbronze.com>
2012-05-11 21:33:10 +0800

    Added password service to password controller.
    Signed-off-by: Alec Gonzales <alec.gonzales@orangeandbronze.com>

--- src/main/java/com/onb/otp/controller/PasswordController.java
+++ src/main/java/com/onb/otp/controller/PasswordController.java
@@ -1,14 +1,20 @@
 package com.onb.otp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.onb.otp.service.impl.PasswordServiceImpl;
+
 @Controller
 public class PasswordController {
 	
+	@Autowired
+	PasswordServiceImpl passwordService;
+	
 	@RequestMapping(value="/create/otp-list", method=RequestMethod.GET) 
 	public void generateOtp() {
-		System.out.println("here");
+		System.out.println("weh");
 	}
 }
