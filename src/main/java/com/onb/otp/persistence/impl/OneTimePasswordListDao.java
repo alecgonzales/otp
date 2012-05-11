b4b1278d3b1225f6550e0a3dfbaca6c68b4a0ad1
Alec Gonzales <alec.gonzales@orangeandbronze.com>
2012-05-12 00:22:16 +0800

    Added transactional to dao.
    Signed-off-by: Alec Gonzales <alec.gonzales@orangeandbronze.com>

--- src/main/java/com/onb/otp/persistence/impl/OneTimePasswordListDao.java
+++ src/main/java/com/onb/otp/persistence/impl/OneTimePasswordListDao.java
@@ -3,12 +3,14 @@ package com.onb.otp.persistence.impl;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.onb.otp.domain.OneTimePasswordList;
 import com.onb.otp.persistence.base.BaseDao;
 import com.onb.otp.persistence.base.OneTimePasswordListDaoBase;
 
 @Repository
+@Transactional
 public class OneTimePasswordListDao extends BaseDao implements OneTimePasswordListDaoBase {
 	@Autowired
 	public OneTimePasswordListDao(SessionFactory sessionFactory) {
