2c76d04ded712c324616699a18beaf62878c9926
Alec Gonzales <alec.gonzales@orangeandbronze.com>
2012-05-14 15:02:15 +0800

    Added user property to one time password.
    Signed-off-by: Alec Gonzales <alec.gonzales@orangeandbronze.com>

--- src/main/java/com/onb/otp/domain/OneTimePasswordList.java
+++ src/main/java/com/onb/otp/domain/OneTimePasswordList.java
@@ -8,7 +8,11 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
 
 @Entity
 @Table(name="one_time_password_list")
@@ -19,7 +23,12 @@ public class OneTimePasswordList {
 	private Long id;
 	
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "passwordList")
-	public Set<OneTimePassword> passwords;
+	@Cascade({CascadeType.SAVE_UPDATE})
+	private Set<OneTimePassword> passwords;
+	
+	@OneToOne
+	private User user;
+	
 
 	public Long getId() {
 		return id;
@@ -32,5 +41,11 @@ public class OneTimePasswordList {
 	}
 	public void setPasswords(Set<OneTimePassword> passwords) {
 		this.passwords = passwords;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
 	}
 }
