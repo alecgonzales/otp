be1150053a4c67f7005a396315ff38df1bc3cb17
Alec Gonzales <alec.gonzales@orangeandbronze.com>
2012-05-17 00:20:10 +0800

    Removed jaxb annotations from batch.
    Signed-off-by: Alec Gonzales <alec.gonzales@orangeandbronze.com>

--- src/main/java/com/onb/otp/domain/OneTimePasswordListBatch.java
+++ src/main/java/com/onb/otp/domain/OneTimePasswordListBatch.java
@@ -9,15 +9,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
 
-@XmlRootElement(name = "batch_response")
 @Entity
 @Table(name="one_time_password_list_batch")
 public class OneTimePasswordListBatch {
@@ -36,21 +31,18 @@ public class OneTimePasswordListBatch {
 	public Long getId() {
 		return id;
 	}
-	@XmlTransient
 	public void setId(Long id) {
 		this.id = id;
 	}
 	public Set<OneTimePasswordList> getPasswordLists() {
 		return passwordLists;
 	}
-	@XmlElement(name="otp-list")
 	public void setPasswordLists(Set<OneTimePasswordList> passwordLists) {
 		this.passwordLists = passwordLists;
 	}
 	public int getBatchSize() {
 		return batchSize;
 	}
-	@XmlAttribute(name="batch-size")
 	public void setBatchSize(int batchSize) {
 		this.batchSize = batchSize;
 	}
