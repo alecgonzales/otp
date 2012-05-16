c7a415bf373f1d0d18382ba84e6cea15cb99aa45
Alec Gonzales <alec.gonzales@orangeandbronze.com>
2012-05-16 22:34:48 +0800

    Fixed data fixtures.
    Signed-off-by: Alec Gonzales <alec.gonzales@orangeandbronze.com>

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
 insert into status (value) values ("free");
 insert into status (value) values ("associated");
-insert into status (value) values ("activated");
+insert into status (value) values ("active");
 
-insert into user (username) values ("user");
+insert into user (username, status_id) values ("user", 1);
