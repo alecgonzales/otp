745ef1d60f63439c54a72b43f35aa181d9fedd6d
Alec Gonzales <alec.gonzales@orangeandbronze.com>
2012-05-16 21:52:02 +0800

    Removed unused import.
    Signed-off-by: Alec Gonzales <alec.gonzales@orangeandbronze.com>

--- src/main/java/com/onb/otp/domain/OneTimePasswordList.java
+++ src/main/java/com/onb/otp/domain/OneTimePasswordList.java
@@ -14,7 +14,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
