1befbb5185eb55afd5125f9d2cc0802a894fcd3d
Alec Gonzales <alec.gonzales@orangeandbronze.com>
2012-05-16 19:57:47 +0800

    Added import.sql to initialize fixtures.
    Signed-off-by: Alec Gonzales <alec.gonzales@orangeandbronze.com>

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into status (value) values ("free");
+insert into status (value) values ("associated");
+insert into status (value) values ("activated");
+
+insert into user (username) values ("user");
