86f0757d79e89010d580e25c1aff568aeea24bfc
Alec Gonzales <alec.gonzales@orangeandbronze.com>
2012-05-17 21:07:32 +0800

    Fixed import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-insert into status (value, referenceIndex, remaining) values ("free", "B1", 1);
-insert into status (value, referenceIndex, remaining) values ("associated", "B1", 2);
-insert into status (value, referenceIndex, remaining) values ("active", "B1", 3);
+insert into status (value, reference_index, remaining) values ("free", "B1", 1);
+insert into status (value, reference_index, remaining) values ("associated", "B1", 2);
+insert into status (value, reference_index, remaining) values ("active", "B1", 3);
 
 insert into user (username, status_id) values ("user", 1);
