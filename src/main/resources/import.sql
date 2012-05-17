9db6afeb7ff26e60ffdf0cfb216290264b640edd
Alec Gonzales <alec.gonzales@orangeandbronze.com>
2012-05-17 17:24:56 +0800

    Added index to status.
    Signed-off-by: Alec Gonzales <alec.gonzales@orangeandbronze.com>

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-insert into status (value) values ("free");
-insert into status (value) values ("associated");
-insert into status (value) values ("active");
+insert into status (value, referenceIndex) values ("free", "B1");
+insert into status (value, referenceIndex) values ("associated", "B1");
+insert into status (value, referenceIndex) values ("active", "B1");
 
 insert into user (username, status_id) values ("user", 1);
