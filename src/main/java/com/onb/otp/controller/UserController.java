d751db5aac04d4995ec7557f951353ba4eee6e70
Alec Gonzales <alec.gonzales@orangeandbronze.com>
2012-05-17 11:08:50 +0800

    Fixed user controller.
    Signed-off-by: Alec Gonzales <alec.gonzales@orangeandbronze.com>

---
+++ src/main/java/com/onb/otp/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.onb.otp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.onb.otp.datatransferobject.UserForLookupUser;
+import com.onb.otp.exception.InvalidRequestParameterException;
+import com.onb.otp.service.base.UserServiceBase;
+
+@Controller
+public class UserController {
+	@Autowired
+	UserServiceBase userService;
+	
+	@RequestMapping(value="/user-info/_query", method=RequestMethod.GET, params="uniqueID")
+	public @ResponseBody UserForLookupUser lookupUserWithUsername(@RequestParam String uniqueID) throws InvalidRequestParameterException {
+		return new UserForLookupUser(userService.lookupUserByUsername(uniqueID));
+	}
+}
