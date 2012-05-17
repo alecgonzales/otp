f7880889ff3c76e7fc5f4b070bd0e1765e33ac4f
Danilo Lao <danilo.lao@orangeandbronze.com>
2012-05-17 18:37:26 +0800

    added a fixture data.
    Signed-off-by: Danilo Lao <danilo.lao@orangeandbronze.com>

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-insert into status (value, referenceIndex) values ("free", "B1");
-insert into status (value, referenceIndex) values ("associated", "B1");
-insert into status (value, referenceIndex) values ("active", "B1");
+insert into status (value, referenceIndex, remaining) values ("free", "B1", 1);
+insert into status (value, referenceIndex, remaining) values ("associated", "B1", 2);
+insert into status (value, referenceIndex, remaining) values ("active", "B1", 3);
 
 insert into user (username, status_id) values ("user", 1);
